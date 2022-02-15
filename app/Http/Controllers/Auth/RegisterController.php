c143d54790ea77ee054470d939d2f200ee5da25c
juan rojas <carlos3434@hotmail.com>
2022-02-14 22:11:24 -0500

    seed database

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,6 +8,7 @@ use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 use Illuminate\Http\Request;
 use App\Models\User;
+use App\Models\Order;
 
 class RegisterController extends Controller
 {
